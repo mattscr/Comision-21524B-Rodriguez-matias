d867217fa4fb14040e3209ff83c15484031e5650
Matias <matias.rodriguez.e@gmail.com>
2023-09-13 10:26:59 -0300

    CRUD finalizado

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -12,4 +12,32 @@ const getPostbyId = async (req, res) =>{
 
     res.json(postId)
 }
-module.exports = {getAllPost, getPostbyId}
+
+const crearNuevoPost = async (req, res) => {
+    const { titulo, contenido, link_img } = req.body
+
+    await PostModel.create({ titulo, contenido, link_img })
+
+    res.send('post creado con exito')
+}
+
+const updatePost = async (req, res) => {
+    const id = req.params.id
+    const { titulo, contenido, link_img } = req.body
+
+    await PostModel.update({ titulo, contenido, link_img }, {
+        where: { id: id }
+    })
+
+    res.send('post editado con exito')
+}
+
+const deletePost = async (req, res) => {
+    const id = req.params.id
+    await PostModel.destroy({
+        where : {id: id}
+    })
+
+    res.send('post eliminado con exito')
+}
+module.exports = {getAllPost, getPostbyId, crearNuevoPost, updatePost, deletePost}
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,9 +1,12 @@
 const { Router } = require("express")
-const { getAllPost, getPostbyId } = require ('../controllers/posts.controllers')
+const { getAllPost, getPostbyId, crearNuevoPost, updatePost, deletePost } = require ('../controllers/posts.controllers')
 
 const router = Router()
 
 router.get('/', getAllPost)
 router.get('/:id', getPostbyId)
+router.post('/', crearNuevoPost)
+router.put('/:id', updatePost)
+router.delete('/:id', deletePost)
 
 module.exports = router
